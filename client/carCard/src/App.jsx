6ea1ce2b0ec2b98330fcceed49574b071c3f2b85
TothKristof <56380808+TothKristof@users.noreply.github.com>
2024-11-18 22:16:54 +0100

    Half done design

--- client/carCard/src/App.jsx
+++ client/carCard/src/App.jsx
@@ -5,7 +5,7 @@ import Drawing from "./pages/Drawing/Drawing";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CollectData from "./pages/CollectData/CollectData";
 import HeadLine from "./components/Headline/HeadLine";
-import Encounter from "./components/Encounter";
+import Encounter from "./pages/Encounter/Encounter";
 
 function App() {
   const [isPlayerTurn, setIsPlayerTurn] = useState(true);
@@ -24,48 +24,6 @@ function App() {
     playerScore: null,
     message: "Start game",
   });
-
-  function updateCards() {
-    const yourCardsTest = [...yourCards];
-    const aiCardsTest = [...aiCards];
-    const yourTalonTest = [...yourTalon];
-    const aiTalonTest = [...aiTalon];
-    yourCardsTest.splice(yourCardsTest.indexOf(playerSelectedCard), 1);
-    aiCardsTest.splice(aiCardsTest.indexOf(aiSelectedCard), 1);
-    yourTalonTest.length > 0 &&
-      yourCardsTest.push(...yourTalonTest.splice(0, 1));
-    aiTalonTest.length > 0 && aiCardsTest.push(...aiTalonTest.splice(0, 1));
-    setAiCards(null);
-    setPlayerSelectedCard(null);
-    setYourCards(yourCardsTest);
-    setAiCards(aiCardsTest);
-    setYourTalon(yourTalonTest);
-    setAiTalon(aiTalonTest);
-  }
-
-  function handleEncounter() {
-    if (
-      ["acceleration", "consumption", "weight"].includes(selectedCarAttribute)
-    ) {
-      aiSelectedCard[selectedCarAttribute] -
-        playerSelectedCard[selectedCarAttribute] >
-      0
-        ? setPlayerScore(() => playerScore + 1)
-        : setEnemyScore(() => enemyScore + 1);
-    } else {
-      playerSelectedCard[selectedCarAttribute] -
-        aiSelectedCard[selectedCarAttribute] >
-      0
-        ? setPlayerScore(() => playerScore + 1)
-        : setEnemyScore(() => enemyScore + 1);
-    }
-    if (yourCards.length === 1) {
-      setPhase("result");
-    } else {
-      updateCards();
-      setPhase("collect data");
-    }
-  }
 
   return (
     <>
