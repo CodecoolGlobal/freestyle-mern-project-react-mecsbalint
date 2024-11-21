329bd708c32e6f165a0fd53fd99bb0d5bffea340
TothKristof <56380808+TothKristof@users.noreply.github.com>
2024-11-21 17:46:14 +0100

    Bugfix login

--- client/carCard/src/components/UserForm/UserForm.jsx
+++ client/carCard/src/components/UserForm/UserForm.jsx
@@ -7,22 +7,26 @@ function UserForm({ onSubmitHandler, type }) {
   return (
     <>
       <h3>{type === "login" ? "Login" : "Sign Up"}</h3>
-      <Form>
+      <Form onSubmit={onSubmitHandler}>
+        {type === "signUp" && (
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Name</Form.Label>
+            <Form.Control name="name" type="text" placeholder="Enter name" />
+          </Form.Group>
+        )}
+
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control name="name" type="text" placeholder="Enter name" />
-        </Form.Group>
-        {
-          type === 'signUp' &&
-         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control name="email" type="email" placeholder="Enter email" />
         </Form.Group>
-        }
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control name="password" type="password" placeholder="Enter Password" />
+          <Form.Control
+            name="password"
+            type="password"
+            placeholder="Enter Password"
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           {type === "login" ? "Login" : "Sign Up"}
